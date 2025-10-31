8c3235958bfa450028c986a1e72755019afe7a07
jadiel.luis@hotmail.com <jadiel.luis@hotmail.com>
2025-10-31 12:33:11 -0300

    Adicionando agrupamento de dados com .groupby() em Pandas

--- analise_vendas.py
+++ analise_vendas.py
@@ -23,3 +23,28 @@ print(df_vendas[['Produto', 'Preco']])
 # 6. Média de Preço (Primeira análise estatística!)
 print("\nMédia de Preços:")
 print(df_vendas['Preco'].mean())
+# 7. FILTROS CONDICIONAIS
+print("\nProdutos com Preço Superior a R$80.00:")
+
+# Cria uma "Máscara" Booleana (True/False)
+# A variável 'preco_alto' será True para preços > 80.00 e False para o restante
+preco_alto = df_vendas['Preco'] > 80.00
+
+# Aplica a máscara ao DataFrame original para mostrar apenas as linhas True
+df_filtrado = df_vendas[preco_alto]
+
+# Imprime o novo DataFrame filtrado
+print(df_filtrado)
+# 8. AGRUPAMENTO DE DADOS COM .groupby()
+
+# Objetivo: Calcular a média de Preço por Produto.
+print("\nPreço Médio por Produto (Agrupamento):")
+
+# 1. Agrupar o DataFrame pela coluna 'Produto'
+df_agrupado = df_vendas.groupby('Produto')
+
+# 2. Aplicar a função de média (.mean()) à coluna 'Preco' dentro de cada grupo
+media_por_produto = df_agrupado['Preco'].mean()
+
+# 3. Imprimir o resultado
+print(media_por_produto)
